4315f7ee019af963e5a8dfb976f6eadb0f4a7bde
zoey <13712053580@163.com>
2019-04-15 14:16:55 +0800

    添加销售变更页面

--- src/pages/saleDetails/saleDetails.js
+++ src/pages/saleDetails/saleDetails.js
@@ -1,22 +1,68 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {getCustomer} from '../../redux/actions'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getCustomer } from '../../redux/actions'
+import './saleDetails.css'
+
 
 import {
-    List,
-   
-  } from 'antd-mobile'
+    List, NavBar, Card, WhiteSpace
+
+} from 'antd-mobile'
 
 
-  class SaleDetails extends Component{
-      render(){
-          return(
-              <div>xinzengkehu</div>
-          )
-      }
-  }
+class SaleDetails extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar>销售变更</NavBar>
+                <Card.Body>
+                    <div className='saleItem'>
+                        <div className='customerName'>客户名称：</div>
+                        <div className='content'>深圳市夜听文化传播有限公司</div>
+                    </div>
+                    <WhiteSpace/>
+                    <div className='saleItem'>
+                        <div className='customerName'>等级：</div>
+                        <div className='content'>中</div>
+                    </div>
+                    <WhiteSpace/>
+                    <div className='saleItem'>
+                        <div className='customerName'>责任人：</div>
+                        <div className='content'>江小白</div>
+                    </div>
+                    <WhiteSpace/>
+                    <div className='saleItem'>
+                        <div className='customerName'>销售阶段：</div>
+                        <div className='content'>面谈</div>
+                    </div>
+                    <WhiteSpace/>
+                    <div className='saleItem'>
+                        <div className='customerName'>状态：</div>
+                        <div className='content'>正常</div>
+                    </div>
+                    <WhiteSpace/>
+                    <div className='saleItem'>
+                        <div className='customerName'>来源：</div>
+                        <div className='content'>推荐</div>
+                    </div>
+                    <WhiteSpace/>
+                    <div className='saleItem'>
+                        <div className='customerName'>创建日期：</div>
+                        <div className='content'>2019-4-12</div>
+                    </div>
+                    <WhiteSpace/>
+                    <div className='saleItem'>
+                        <div className='customerName2'>当前状态已停留：</div>
+                        <div>0天</div>
+                    </div>                   
+                </Card.Body>
+                <div className='changInfo'>变更信息</div>
+            </div>
+        )
+    }
+}
 
-  export default connect(
-    state => ({customer: state.customer}),
-    {getCustomer}
-  )(SaleDetails)
+export default connect(
+    state => ({ customer: state.customer }),
+    { getCustomer }
+)(SaleDetails)
